881576125d2b28f1f0b98700e44aa22a278a9f12
Cse19455 <59655496+Cse19455@users.noreply.github.com>
2020-01-08 15:39:12 +0100

    Initial commit

---
+++ JAVASC01.js
@@ -0,0 +1,25 @@
+function namener() {
+    var ihrname = prompt("Bitte Ihren Namen\neingeben:", "Name");
+    if (ihrname != null) {
+        alert("Sie haben eingegeben:\n" + ihrname + "\nVielen Dank");
+        return name;
+    }
+}
+
+function summe(a, b, ...restliche) {
+    var summe = a + b;
+    for (i = 0; i < restliche.length; i++) {
+        summe += restliche[i];
+    }
+    return summe;
+}
+
+function name() {
+    var1 = window.outerHeight;
+    var2 = window.outerWidth;
+    document.getElementById("size").innerHTML = "Fenstergröße: " + var1 + " x " + var2;
+}
+
+$(document).ready(function() {
+    name();
+})
